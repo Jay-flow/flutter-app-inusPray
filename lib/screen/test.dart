93f565e23003d0a88eb63c7426254004f1de0b37
J-flow <kimpetro153@gmail.com>
2020-07-06 21:33:29 +0900

    Temp save

--- lib/screen/test.dart
+++ lib/screen/test.dart
@@ -7,22 +7,36 @@ class Test extends StatefulWidget {
   _TestState createState() => _TestState();
 }
 
-class _TestState extends State<Test> {
+class _TestState extends State<Test> with SingleTickerProviderStateMixin {
   _showOverlay() async {
+    Tween<double> tweenOpacity = Tween<double>(begin: 0, end: 1);
+    AnimationController controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+    Animation<double> animation = CurvedAnimation(
+      parent: controller,
+      curve: Curves.decelerate,
+    );
+    Animation<double> animationOpacity = tweenOpacity.animate(animation);
+
     OverlayState overlayState = Overlay.of(context);
     OverlayEntry overlayEntry = OverlayEntry(
-      builder: (context) => Positioned(
-        top: 30,
-        left: 10,
-        right: 10,
-        child: Container(
-          child: Card(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'Hello world',
-                style: TextStyle(
-                  fontSize: 15,
+      builder: (context) => FadeTransition(
+        opacity: animationOpacity,
+        child: Positioned(
+          top: 30,
+          left: 10,
+          right: 10,
+          child: Container(
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Hello world',
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
                 ),
               ),
             ),
